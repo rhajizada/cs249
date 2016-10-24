ffa5e35936a3163da47e900f9533d4ed34b4766c
Rufat Hajizada <vice.genius@gmail.com>
2016-10-24 13:31:53 -0400

    5_1 changed

--- ghp5/ghp5_1/ghp5_1.java
+++ ghp5/ghp5_1/ghp5_1.java
@@ -1,13 +1,13 @@
 public class ghp5_1 {
 
     public static void main(String[] args) {
-	    Circle2D circle1 = new Circle2D(0, 0, 4);
-        Circle2D circle2 = new Circle2D(0, 0, 3);
-        Circle2D circle3 = new Circle2D(5, 5, 1);
-        System.out.println("Area of circle1: " + circle1.getArea());
-        System.out.println("Perimeter of circle1: " + circle1.getPerimeter());
-        System.out.println("Circle1 contains (2,2)? " + circle1.contains(2, 2) );
-        System.out.println("Circle1 contains circle2? " + circle1.contains(circle2));
-        System.out.println("Circle1 overlapses circle3? " + circle1.overlaps(circle3));
+	    Circle2D c1 = new Circle2D(0, 0, 4);
+        Circle2D c2 = new Circle2D(0, 0, 3);
+        Circle2D c3 = new Circle2D(5, 5, 1);
+        System.out.println("Area of circle1: " + c1.getArea());
+        System.out.println("Perimeter of circle1: " + c1.getPerimeter());
+        System.out.println("Circle1 contains (2,2)? " + c1.contains(2, 2) );
+        System.out.println("Circle1 contains circle2? " + c1.contains(circle2));
+        System.out.println("Circle1 overlapses circle3? " + c1.overlaps(circle3));
     }
 }
