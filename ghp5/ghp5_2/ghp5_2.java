d494597c75743e29485f99af32fe87f46d78f98b
Rufat Hajizada <vice.genius@gmail.com>
2016-10-24 13:30:43 -0400

    Fixed

--- ghp5/ghp5_2/ghp5_2.java
+++ ghp5/ghp5_2/ghp5_2.java
@@ -4,10 +4,10 @@ public class ghp5_2 {
         Player player = new Player(100, 100);
         player.getCurrentWeapon().printWeapon();
         Weapon sword = new Weapon("Common Sword", 50);
+        player.setWeapon(sword);
         player.getCurrentWeapon().printWeapon();
         Weapon coolSword = new Weapon("Glamdring, the Foe-Hammer", 1500);
+        player.setWeapon(coolSword);
         player.getCurrentWeapon().printWeapon();
-
-
     }
 }
